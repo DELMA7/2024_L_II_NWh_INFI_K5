e7c9db77d8a568e6d74256158451fe92432d3648
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-23 11:51:47 +0100

    poprawka_imienia

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Patryk_81475", "mgs":Hello World!"}', rv.data)
